48434a38a7b1f36d877d885547bcc09f26b5e797
Chloe Brown <chloemail@protonmail.com>
2022-04-14 10:32:24 +0100

    getPokemon updates

--- index.js
+++ index.js
@@ -7,50 +7,53 @@ const PORT = 4000
 // POKEMON API
 const pokemonApiUrl = 'https://pokeapi.co/api/v2'
 
-const getPokemon = async () => {
-  // ? get the pokemon
+const getPokemon = async (pokeName) => {
+  // get the pokemon
   try {
+    // GETS ALL THE POKEMON AND THEN WE FIND THE ONE WE ARE PASSING WITH POKENAME
     const { data } = await axios.get(`${pokemonApiUrl}/pokemon`, {
       params: {
         offset: 0,
-        limit: 10,
+        limit: 5,
       },
     })
-    // console.log('pokemonData-->', data)
-    // ? take the pokemon name as parameter and return the description
-    const getDescription = async (pokeName) => {
-      try {
-        const { data } = await axios.get(`${pokemonApiUrl}/pokemon-species/` + pokeName, {
-          params: {
-            offset: 0,
-            limit: 20,
-          },
-        })
-        console.log('Description: ', data.flavor_text_entries[1].flavor_text)
-      } catch (error) {
-        console.log(error)
-        // return ({ status: 404, json: {error: error.message}})
-      }
-    }
-    getDescription('pikachu')
-    getDescription('charmander')
-
-    // ? store the data needed for each pokemon
-    const pokeObj = data.results.map((poke, index) => {
+    // get pokemon data for single pokemon
+    const pokeObjectSet = data.results.map((poke, index) => {
       return {
         name: poke.name,
         url: poke.url,
         image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png`,
         id: index + 1,
       }
+  })
+console.log('pokeObjectSet', pokeObjectSet)
+const singlePokeObj = data.results.find((poke, index) => {
+  console.log('FINDING', pokeName, poke.name)
+  if (poke.name === pokeName) {
+    return {
+      name: poke.name,
+      url: poke.url,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png`,
+      id: index + 1,
+    }
+  }
+}) 
+    // console.log('pokemonData-->', data)
+    // take the pokemon name as parameter and return the description
+    const descriptionData = await axios.get(`${pokemonApiUrl}/pokemon-species/` + pokeName, {
+      params: {
+        offset: 0,
+        limit: 20,
+      },
     })
-    console.log(pokeObj)
+    // console.log('Description: ', data.flavor_text_entries[1].flavor_text)
+const pokeDescription = descriptionData.flavor_text_entries[1].flavor_text
+console.log('pokeDescription', pokeDescription)
   } catch (error) {
     console.log(error)
-    // return ({ status: 404, json: {error: error.message}})
   }
 }
-const pokeData = getPokemon('charizard')
+const pokeData = getPokemon('charmander')
 console.log('pokeData', pokeData)
 
 // MIDDLEWARE

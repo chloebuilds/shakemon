f23d10203c40f8e0dcabbf32975178b43a31d45c
Chloe Brown <chloemail@protonmail.com>
2022-04-14 09:52:23 +0100

    storing required pokemon data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express' 
+import express from 'express'
 import axios from 'axios'
 
 const app = express()
@@ -8,15 +8,34 @@ const PORT = 4000
 const pokemonApiUrl = 'https://pokeapi.co/api/v2'
 
 const getPokemon = async () => {
-  // get the pokemon
+  // ? get the pokemon
   try {
     const { data } = await axios.get(`${pokemonApiUrl}/pokemon`, {
       params: {
         offset: 0,
-        limit: 20,
-      }
+        limit: 10,
+      },
     })
-    console.log('pokemonData-->', data)
+    // console.log('pokemonData-->', data)
+    // ? take the pokemon name as parameter and return the description
+    const getDescription = async (pokeName) => {
+      try {
+        const { data } = await axios.get(`${pokemonApiUrl}/pokemon-species/` + pokeName, {
+          params: {
+            offset: 0,
+            limit: 20,
+          },
+        })
+        console.log('Description: ', data.flavor_text_entries[1].flavor_text)
+      } catch (error) {
+        console.log(error)
+        // return ({ status: 404, json: {error: error.message}})
+      }
+    }
+    getDescription('pikachu')
+    getDescription('charmander')
+
+    // ? store the data needed for each pokemon
     const pokeObj = data.results.map((poke, index) => {
       return {
         name: poke.name,
@@ -25,29 +44,14 @@ const getPokemon = async () => {
         id: index + 1,
       }
     })
+    console.log(pokeObj)
   } catch (error) {
     console.log(error)
+    // return ({ status: 404, json: {error: error.message}})
   }
 }
-getPokemon()
-
-// take the pokemon name as parameter and return the description
-const getDescription = async (pokeName) => {
-try {
-  const { data } = await axios.get(`${pokemonApiUrl}/pokemon-species/`  + pokeName, {
-    params: {
-      offset: 0,
-      limit: 20,
-    }
-  })
-  console.log('Description: ', data.flavor_text_entries[1].flavor_text)
-} catch (error) {
-  console.log(error)
-}
-}
-getDescription('pikachu')
-getDescription('charmander')
-
+const pokeData = getPokemon('charizard')
+console.log('pokeData', pokeData)
 
 // MIDDLEWARE
 app.use(express.json())
@@ -56,21 +60,17 @@ app.use(express.json())
 const router = express.Router()
 
 // GOTTA CATCH EM ALL
-router.route('/pokemon/:id')
-  .get(getPokemon)
-
-
+router.route('/pokemon/:id').get(getPokemon)
 
 // CATCH ALL RESPONSE
 app.use((_req, res) => {
-  return res.status(404).json({ message: 'Route not found'})
+  return res.status(404).json({ message: 'Route not found' })
 })
 
 // LOGGER
 app.use((req, _res, next) => {
   console.log(`🪶📜 Request for pokemon received: ${req.method} - ${req.url}`)
-  next() 
+  next()
 })
 
 app.listen(PORT, () => console.log(`🤖 Server running on port ${PORT}`))
-

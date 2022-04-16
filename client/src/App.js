e1a8ca7eea0df4da9fde257b3ed351575622d8c4
Chloe Brown <chloemail@protonmail.com>
2022-04-16 23:14:40 +0100

    pokemon search with react-select, handleLoadOptions, handleChange

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { useEffect } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
+import PokeSearch from './components/PokeSearch'
 
 function App() {
 
-  const id = 6
+  // const id = 25
 
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get(`/pokemon/${id}`) 
-      console.log(data)
-    }
-    getData()
-  })
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const { data } = await axios.get(`/pokemon/${id}`) 
+  //     console.log(data)
+  //   }
+  //   getData()
+  // })
 
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1>Shakemon</h1>
+      <PokeSearch />
+    </>
+  )
 }
 
 export default App
---
+++ client/src/components/PokeSearch.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import axios from 'axios'
+import AsyncSelect from 'react-select/async'
+
+const initialData = {
+  name: '',
+  sprite: '',
+  description: ''
+}
+
+function PokeSearch() {
+
+  const [pokeData, setPokeData] = React.useState(initialData)
+  const [error, setError] = React.useState(null)
+  
+  const handleLoadOptions = async (inputValue) => {
+    setError('')
+    console.log(inputValue)
+    try {
+      const { data } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon-species/${inputValue}`
+      )
+      console.log('data from pokemon API -->', data)
+      if (data.Response === 'False') {
+        return []
+      }
+      return Array({ // react-select needs an array to map over the options
+        label: data.name,
+        value: `${data.name} ${data.id}`
+      })
+    } catch (err) {
+      console.log(err)
+      setError('Could not find the pokemon')
+    }
+  }
+
+  const handleChange = async ({ value: id }) => {
+    // on change, make the request to the back end with the id of the pokemon from pokemon API
+    console.log('id', id)
+    const { data } = await axios.get(
+      `/pokemon/${id}`
+    )
+    console.log('pokemon data', data)
+    // if there is no response, set an error
+    if (data.Response === 'False') {
+      setError('There was an error setting the data')
+      return
+  }
+  // set the pokemon data with the data from the backend with the shakespeare description
+  setPokeData({
+    name: data.name,
+    sprite: data.sprite,
+    description: data.description
+  })
+  console.log(pokeData)
+}
+
+// ! still need to display error messages
+  return  (
+    <>
+      <h4>Pokemon Search</h4>
+      <p> Start typing the name of the pokemon you are searching for below and see Shakespeare describe your chosen pokemon.</p>
+      <AsyncSelect 
+        placeholder="Type to find a pokemon.." 
+        loadOptions={handleLoadOptions} 
+        onChange={handleChange} 
+        />
+      <div>
+        <p>Name: {pokeData.name}</p>
+        <p>Sprite: {pokeData.sprite}</p>
+        <p>Description: {pokeData.description}</p>
+      </div>
+      </>
+  )
+}
+
+export default PokeSearch

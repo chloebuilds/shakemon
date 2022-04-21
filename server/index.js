c21691f3ae0ae398c723d2811e15b159be6a4d2b
Chloe Brown <chloemail@protonmail.com>
2022-04-21 06:41:57 +0100

    getPokemon test running

--- server/index.js
+++ server/index.js
@@ -53,3 +53,5 @@ app.use((req, _res, next) => {
   console.log(`🪶📜 Request for pokemon received: ${req.method} - ${req.url}`)
   next()
 })
+
+module.exports = app
--- server/server.js
+++ server/server.js
@@ -3,4 +3,3 @@ const PORT = 4000
 
 app.listen(PORT, () => console.log(`🤖 Server running on port ${PORT}`))
 
-module.exports = app

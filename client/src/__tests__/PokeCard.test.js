64afed4c85e29d855734b435c5bb487461af00c7
Chloe Brown <chloemail@protonmail.com>
2022-04-22 09:46:04 +0100

    testing on frontend

--- client/src/__tests__/PokeCard.test.js
+++ client/src/__tests__/PokeCard.test.js
@@ -1,29 +1,23 @@
-// import axios from 'axios'
-// import { render, screen, waitFor, cleanup } from '@testing-library/react'
-// import PokeCard from '../components/PokemonCard'
+import { cleanup, render } from '@testing-library/react'
+import axios from 'axios'
+import PokeCard from '../components/PokemonCard'
 
-// import { render } from "@testing-library/react"
+afterEach(cleanup)
+
+xtest('Should display the pokeData in the PokeCard in PokeSearch',  () => {
+    const mockPokeData = [
+      {
+        name: 'charizard',
+        sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+        description: 'Spits fire yond is hot enow to melt boulders. Known to cause forest fires unintentionally.',
+      },
+    ]
+
+    axios.get.mockResolvedValueOnce({ data: mockPokeData })
+
+    render(<PokeCard pokeData={mockPokeData} />)
 
 
-// test('Should accept user input and display it correctly', () => {
-//   render(<PokeCard />)
-// })
+  }
+)
 
-
-
-
-// function PokemonCard({ pokeData }) {
-
-//   return (
-//     <section className="pokemon-card-display">
-//       <div className='pokemon-card'>
-//         <h3>{pokeData.name}</h3>
-//         <img src={pokeData.sprite} alt={pokeData.name}/>
-//         <p>{pokeData.description}</p>
-//       </div>
-//       <div>
-//         <img src={CardBack} alt="Pokemon Card Back"/>
-//       </div>
-//     </section>
-//   )
-// }
--- client/src/__tests__/PokeSearch.test.js
+++ client/src/__tests__/PokeSearch.test.js
@@ -1,7 +1,53 @@
-import { render } from '@testing-library/react'
-import PokeCard from '../components/PokemonCard'
+import React from 'react'
+import { cleanup, render, screen, waitFor } from '@testing-library/react'
+import axios from 'axios'
+import Loader from '../components/Loader'
 
 
-test('Should accept user input and display it correctly', () => {
-  render(<PokeCard />)
+import Select from 'react-select'
+import selectEvent from 'react-select-event'
+import PokeSearch from '../components/PokeSearch'
+
+afterEach(cleanup)
+
+// test that the loader component renders on loading data
+test('Should render loading state', 
+  async () => {
+    axios.get.mockRejectedValueOnce()
+
+    render(
+        <Loader />
+    )
+
+    const loadingIndicator = screen.getByRole('pokemon-loading')
+
+    expect(loadingIndicator).toBeInTheDocument()
+
+    await waitFor(() => expect(axios.get).toHaveBeenCalled())
+  }
+)
+
+xtest('Should render pokeData on select of pokemon', async () => {
+  
+  const mockOptions = ['pikachu', 'charizard']
+  
+  const {getByTestId, getByLabelText} = render(
+    <form data-testid="form">
+      <label htmlFor="pokemon">Pokemon</label>
+      <Select options={mockOptions} name="pokemon" inputId="pokemon" isMulti />
+    </form>,
+  )
+  expect(getByTestId('form')).toHaveFormValues({pokemon: ''}) // empty select
+  
+  // select two values...
+  await selectEvent.select(getByLabelText('Pokemon'), ['pikachu', 'charizard'])
+  expect(getByTestId('form')).toHaveFormValues({pokemon: ['pikachu', 'charizard']})
+  
+  // ...and add a third one
+  await selectEvent.select(getByLabelText('Pokemon'), 'eevee')
+  expect(getByTestId('form')).toHaveFormValues({
+    pokemon: ['pikachu', 'charizard', 'eevee'],
+  })
 })
+
+
